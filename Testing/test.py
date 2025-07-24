73816a5b9a0b08261a2e2feb060c2bf4ce8d060e
Charlene <charlene.hocevar@gmail.com>
2025-07-24 13:19:46 +0200

    startof story

---
+++ Testing/test.py
@@ -0,0 +1,19 @@
+print("         ")
+print("Welcome to The Best Game Ever, my dear friend.")
+print ("")
+print("Your task will be simple: find Schindler and bring him back to Dreamville.")
+print ("")
+print ("Your journey starts in 3... 2... 1...")
+print("")
+print("\"Wait!\" you say? \"I am not ready for the responsibility of such an epic task!\" ?")
+print()
+print ("Well...")
+print()
+print("Few are.Fewer succeed. But those who do... ")
+print("This\nis\n \nfine.")
+print ("""you can
+
+sjsjsj
+
+sksksk
+sjjsjsjs""")
